3e15d4c44f0d22433476d62e8ffbdc8143f2ba40
crystalivel <matibilal2002@gmail.com>
2025-07-22 14:56:47 +0100

    day 3 task

--- day 3/index.js
+++ day 3/index.js
@@ -8,19 +8,112 @@ function factorial(n){
 }
 
 console.log(factorial(16))
-var num = 123456;
-var degits = 0;
+function Digits(num){
+    var digits = 0;
 while (num > 0){
  num = Math.floor(num / 10);
-    degits++
+       digits++
+    
+} 
+return digits;
 }
-console.log(degits)
+console.log(Digits(123456))
 
 let star = ["   *   ","  ***  "," ***** ","*******","   |   "];
 for (let i = 0; i <= star.length; i++){
     let tree ="";
-    tree += star[i] + "<br>";
+    tree += star[i];
     console.log(tree)
 
 }
 console.log(star.length)
+
+function numberToDay(day){
+    var dayIs = "";
+switch (day) {
+    case 1 : 
+        dayIs = "monday";
+    break;
+    case 2 : 
+        dayIs = "tuesday";
+    break; 
+    case 3 : 
+        dayIs = "wednsday";
+    break; 
+    case 4 : 
+        dayIs = "thursday";
+    break;
+    case 5 :
+        dayIs = "friday";
+    break; 
+    case 6 : 
+        dayIs = "saturday";
+    break;
+    case 7 : 
+        dayIs = "sunday";
+    break; 
+    
+}
+return dayIs
+}
+console.log(numberToDay(5))
+
+function Max_a_b_c (a,b,c){
+let Maxuimam = Math.max(a,b,c);
+return Maxuimam
+}
+console.log(Max_a_b_c(13,23.4,0,345))
+function myGrade(score){
+    let note = "";
+if (score > 85){ 
+    note = "A"
+} else if (85 >= score && score > 70)
+  {  note = "B"
+} else if (70 >= score && score > 55){
+    note = "C" 
+} else if (55 >= score && score > 40){
+    note = "D"
+} else if (40 >= score && score > 15){
+    note = "E"
+} else if (score <= 15){
+    note = "F"
+} else {
+    note = Error
+}
+return note;
+}
+console.log(myGrade(83))
+function extendedFactorial (n, p){
+    let nNum;
+    let pNum;
+    let C;
+    nNum = factorial(n)
+    pNum = factorial(p)
+    C = nNum / (pNum * (factorial(n-p)))
+    return C; 
+}
+console.log(extendedFactorial(5,2))
+function calc(num1,ope,num2){
+    let sum;
+    switch (ope){
+        case "+" :
+        sum = num1 + num2;
+        break;
+        case "-":
+        sum = num1 - num2;
+        break;
+        case "*":
+        sum = num1 * num2; 
+        break; 
+        case "/":
+        sum = num1 / num2;
+        break; 
+        case "%":
+        sum = num1 % num2 
+        break;
+        case "c":
+        sum = extendedFactorial(num1,num2);
+        break;
+        
+    }
+    return sum
