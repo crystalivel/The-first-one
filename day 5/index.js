c467a06a1507f4e7b188cb8a0f0fadc3aa049568
crystalivel <matibilal2002@gmail.com>
2025-07-24 11:00:42 +0100

    day 5 task before challenge

---
+++ day 5/index.js
@@ -0,0 +1,34 @@
+const Myperson = {
+    firstName: "Yassine",
+    lastName: "Maati",
+    age: 23,
+    get fullName() {
+        return `${this.firstName} ${this.lastName}`;
+    },
+        set fullName(changedFullname){
+            const [first, last] = changedFullname.split(" ");
+            this.firstName = first;
+            this.lastName = last;
+ 
+        }
+
+}
+    class person {
+        constructor(name,age){
+            this.name = name; 
+            this.age = age;
+        }
+        compareAge(otherPerson){
+            if (this.age > otherPerson.age){
+                return `${this.name} is older than ${otherPerson.name}`
+            }else if(this.age < otherPerson.age) {  
+                return `${otherPerson.name} is older than ${this.name}`
+            } else 
+                return `they are the same age`
+        }
+    }
+    const p1 = new person("john adams",20);
+    const p2 = new person("lily adams",32);
+    const p3 = new person("pere cottard",46); 
+
+    
