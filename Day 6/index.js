7e70d104c6bd0a4d936da5601423b3d8928b7e19
crystalivel <matibilal2002@gmail.com>
2025-07-25 11:16:28 +0100

    codewars day 1 training

--- Day 6/index.js
+++ Day 6/index.js
@@ -1,3 +1,4 @@
+//Usually when you buy something, you're asked whether your credit card number, phone number or answer to your most secret question is still correct. However, since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.function maskify, which changes all but the last four characters into '#'.
 function maskify(cc) { 
     cc = cc.toString();
     let ccLength = cc.length 
@@ -7,7 +8,14 @@ function maskify(cc) {
     if (ccLength < 4 ){
         return cc 
     } else {
-        return hash.repeat(cutlength) + cc.slice(cutlength,ccLength)
+        return hash.repeat(cutlength) + cc.slice(cutlength,ccLength )
     }
 
-}
+}               
+const readline = require('readline');
+
+// Create an interface for reading input
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
