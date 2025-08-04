bde2241fbed74925a44b81023eed7a7a7bab32b4
crystalivel <matibilal2002@gmail.com>
2025-08-04 17:20:39 +0100

    finished the task for day 12

--- day12/index.js
+++ day12/index.js
@@ -4,18 +4,38 @@ const { json } = require('stream/consumers');
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,         }); 
+function seacharry() {
+  rl.question('Input the name of the contact: ', (inputName) => {
+    try {
+      const contactlist = fs.readFileSync('contactlist.json', 'utf-8');
+      const data = JSON.parse(contactlist); // data should be an array of objects
+
+      const person = data.find(
+        (contact) => contact.name.toLowerCase() === inputName.toLowerCase()
+      );
+
+      if (person) {
+        console.log('Contact found:');
+        console.log(person);
+        footer()
+      } else {
+        console.log('Contact not found.');
+        footer()
+      }
+
+    } catch (err) {
+      console.error('Error reading contact list:', err.message);
+      footer()
+    }})}
 // read contactlist 
 function displayInfo(){
-    rl.question(`do you want to read contact ? (y)/(n)`, (Response) => {
-    if (Response === 'y'){
+    console.log(`####### Contact ########`)
             const contactlist = fs.readFileSync('contactlist.json')
             data = JSON.parse(contactlist)
         console.log(data)
-    } 
-    rl.close()
-}
-)
-}
+                    footer()
+    }
+//search contact 
 //save contact 
     function saveContact (contact){
             let data = []
@@ -24,12 +44,12 @@ function displayInfo(){
                 data = JSON.parse(contactlist)
                 data.push(contact)
                 fs.writeFileSync('contactlist.json', JSON.stringify(data, null, 2));
-            } displayInfo()
+            } 
     }
 // collect info 
 function requestInfo(){ 
-rl.question('input the name', (name) => {
-    rl.question(`input the phone number`,(phoneNumber) => {
+rl.question('input the name\n', (name) => {
+    rl.question(`input the phone number\n`,(phoneNumber) => {
         if (isNaN(phoneNumber)){
             rl.setPrompt(`${phoneNumber} is not a number`)
             rl.prompt()
@@ -43,7 +63,8 @@ rl.question('input the name', (name) => {
                     }
                     
                     saveContact(contact)
-                    console.log(contact)
+                    console.log(`${contact.name} contact has been saved`)
+                    footer()
                 } 
             } )
         }else {
@@ -51,12 +72,44 @@ rl.question('input the name', (name) => {
             name : name,
             phoneNumber: phoneNumber
                             }
-            console.log(contact)
-            saveContact(contact)
+                    console.log(`${contact.name} contact has been saved`)
+                    footer()
+                    saveContact(contact)
             }
          
     }) 
 })
 }
-
-requestInfo()
+function mainOp(){
+    rl.question(`#########  Pick a programe: \n 1- Create Contact \n 2- Display Contact \n 3- Search Contact \n 4- Exit \n `,(Response) => {
+        switch (Response){
+            case ('1'): 
+            requestInfo()
+            break;
+            case ('2'):
+            displayInfo()
+            break;
+            case('3'):
+            seacharry()
+            break;
+            case('4'):
+            rl.close()  
+            break;
+            default:
+            console.log(`INVALID INPUT`)     
+            footer()
+            }
+    })
+}
+mainOp()
+    function footer(){
+        rl.question(`0-GO back | 1- Exit`,(Response)=> {
+            switch (Response) {
+                case('0'):
+                mainOp()
+                break;
+                case('1'):
+                rl.close()
+                break;
+            }
+            })}
